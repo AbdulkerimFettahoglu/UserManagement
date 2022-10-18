5d4f5815df2ce1b1999fcc1b29e562d5c29c8b2a
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2022-10-18 15:20:28 +0300

    "loadByUsername" method was specified to show how user should find on the database.

--- src/main/java/com/kerimfettahoglu/usermanagement/service/AppUserService.java
+++ src/main/java/com/kerimfettahoglu/usermanagement/service/AppUserService.java
@@ -1,6 +1,30 @@
 package com.kerimfettahoglu.usermanagement.service;
 
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.kerimfettahoglu.usermanagement.entity.AppUser;
+import com.kerimfettahoglu.usermanagement.repository.AppUserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
-public class AppUserService {}
+@RequiredArgsConstructor
+public class AppUserService implements UserDetailsService {
+
+	private final AppUserRepository appUserRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<AppUser> appUser = Optional.ofNullable(appUserRepository.findByEmail(username));
+		if (appUser.isEmpty()) {
+			throw new UsernameNotFoundException(username);
+		}
+		return appUser.get();
+	}
+
+}
