8b917e7a4fe344bbdc6487e82745c0613e688ac9
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2022-10-19 10:08:44 +0300

    all of the requests coming from clients should be authenticated.

--- src/main/java/com/kerimfettahoglu/usermanagement/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/kerimfettahoglu/usermanagement/configuration/WebSecurityConfiguration.java
@@ -2,13 +2,19 @@ package com.kerimfettahoglu.usermanagement.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.kerimfettahoglu.usermanagement.filter.CustomAuthenticationFilter;
+import com.kerimfettahoglu.usermanagement.filter.CustomAuthorizationFilter;
 import com.kerimfettahoglu.usermanagement.service.AppUserService;
 
 import lombok.AllArgsConstructor;
@@ -23,20 +29,25 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/user/**").permitAll()
-			.anyRequest().authenticated().and().formLogin();
+		http.csrf().disable();
+		// we can disable session creation because we will not use cookies.
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/user").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/login").permitAll();
+		http.authorizeRequests().anyRequest().authenticated();
+		http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+		http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(daoAuthenticationProvider());
+		auth.userDetailsService(appUserService).passwordEncoder(bCryptPasswordEncoder);
 	}
 
 	@Bean
-	public DaoAuthenticationProvider daoAuthenticationProvider() {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setPasswordEncoder(bCryptPasswordEncoder);
-		provider.setUserDetailsService(appUserService);
-		return provider;
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
 	}
 }
