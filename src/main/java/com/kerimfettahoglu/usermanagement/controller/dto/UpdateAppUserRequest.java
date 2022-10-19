6b3efd4a249a6261731ab07c028624547b4483fd
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2022-10-19 12:51:15 +0300

    DTO class was created for update operations.

---
+++ src/main/java/com/kerimfettahoglu/usermanagement/controller/dto/UpdateAppUserRequest.java
@@ -0,0 +1,26 @@
+package com.kerimfettahoglu.usermanagement.controller.dto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class UpdateAppUserRequest {
+	@NotNull(message = "id boş olamaz.")
+	private Long id;
+	@NotEmpty(message = "email adresi boş olamaz.")
+	private String email;
+	@NotEmpty(message = "ilk ad boş olamaz.")
+	private String firstname;
+	@NotEmpty(message = "soyadı boş olamaz.")
+	private String lastname;
+	@NotEmpty(message = "şifre boş olamaz.")
+	private String password;
+}
